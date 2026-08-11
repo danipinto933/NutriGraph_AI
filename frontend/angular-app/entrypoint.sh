bb2ddd252624daa97f666b4416ae0897a3334efa
danipinto933 <danipinto933@gmail.com>
2026-08-11 10:46:55 +0200

    Arreglando problema CORS

--- frontend/angular-app/entrypoint.sh
+++ frontend/angular-app/entrypoint.sh
@@ -5,8 +5,13 @@ export API_URL=${API_URL:-"http://localhost:8000/api/v1"}
 export GRAPH_API_URL=${GRAPH_API_URL:-"http://localhost:8001/api/v1"}
 export CHAT_API_URL=${CHAT_API_URL:-"http://localhost:8002/api/v1"}
 
-# Sustituir variables de entorno en env.template.js y generar env.js final
-envsubst '${API_URL} ${GRAPH_API_URL} ${CHAT_API_URL}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
+# Asegurar que el directorio assets existe
+mkdir -p /usr/share/nginx/html/assets
+
+# Sustituir variables de entorno en env.template.js y generar env.js final si existe la plantilla
+if [ -f /usr/share/nginx/html/assets/env.template.js ]; then
+  envsubst '${API_URL} ${GRAPH_API_URL} ${CHAT_API_URL}' < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
+fi
 
 # Arrancar Nginx en primer plano
 exec nginx -g "daemon off;"
